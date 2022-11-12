c2af0f6a3b30180d4af6591635d54eaaa1152e14
Kshitij Rajale <kshitijrajale@gmail.com>
2022-11-12 11:44:10 +0530

    added basic linked list functionality + tests

--- linked_list.py
+++ linked_list.py
@@ -16,5 +16,59 @@ class LinkedList:
     LinkedList Class
     """
 
-    def __init__(self, head_node: Node) -> None:
-        self.head_node = head_node
+    def __init__(self, head_node_value: int | str | None = None) -> None:
+        """create linked list with given value as head node
+
+        Args:
+            head_node_value (int | str | None, optional): value for head node of the list.
+            Defaults to None.
+        """
+        if head_node_value is None:
+            self.head_node: Node | None = None
+        else:
+            head_node = Node(head_node_value)
+            self.head_node: Node | None = head_node
+
+    def get_head_node(self) -> Node | None:
+        """get head node of linked list
+
+        Returns:
+            Node | None: returns node if present else None
+        """
+        return self.head_node
+
+    def add_new_head(self, head_node_value: int | str) -> Node:
+        """adds new head node to the list
+
+        Args:
+            head_node_value (int | str): value of new head node
+
+        Returns:
+            Node: new head node
+        """
+        new_node: Node = Node(head_node_value)
+        if self.head_node is not None:
+            new_node.set_next_node(self.head_node)
+        self.head_node = new_node
+        return self.head_node
+
+    def append_node(self, new_node_value: int | str) -> Node:
+        """append node with given value at the end of the list
+
+        Args:
+            new_node_value (int | str): value of new node
+
+        Returns:
+            Node: newly appended node
+        """
+        new_node: Node = Node(new_node_value)
+        previous_node = self.head_node
+        if previous_node is None:
+            self.head_node = new_node
+            return self.head_node
+        current_node = previous_node.get_next_node()
+        while current_node:
+            previous_node = current_node
+            current_node = current_node.get_next_node()        
+        previous_node.set_next_node(new_node)
+        return new_node
--- linked_list_test.py
+++ linked_list_test.py
@@ -2,16 +2,165 @@
 LinkedList class unit tests
 
 - __init__
-    - set linked list head node with given head node
+    - set linked list head node as None if no value is provided
+    - set linked list head node as node with provided value
 
 - get_head_node
-    - return head node of linked list
+    - return None if head node does not exist
+    - return head node of linked list if exists
 
 - add_new_head
-    - add given node as new head node
-    - link previous head node after new head node
+    - add node from given value as new head node
+    - set previous head node as next node of new head node if previous head node exists
+    - set none as next node of new head node if does not exists
+    - return new head node
 
-- add_new_node
-    - add the given node at the end of linked list
+- append_node
+    - if list is empty, append node from given_value as head node
+    - append node from given_value at the end of linked list
+    - return newly added node
 
 """
+
+from node import Node
+from linked_list import LinkedList
+
+
+# __init__()
+
+def test_no_head_node() -> None:
+    """set linked list head node as None if no value is provided
+    """
+    linked_list: LinkedList = LinkedList()
+    assert linked_list.head_node is None
+
+def test_set_head_node() -> None:
+    """set linked list head node as node with provided value
+    """
+    given_value: int = 123
+    linked_list: LinkedList = LinkedList(given_value)
+    assert isinstance(linked_list.head_node, Node)
+    assert linked_list.head_node.get_value() == given_value
+    assert linked_list.head_node.get_next_node() is None
+
+
+# get_head_node()
+
+def test_get_no_head_node() -> None:
+    """return None if head node does not exist
+    """
+    linked_list: LinkedList = LinkedList()
+    assert linked_list.get_head_node() is None
+
+def test_get_head_node() -> None:
+    """return head node of linked list
+    """
+    given_value: int = 123
+    linked_list: LinkedList = LinkedList(given_value)
+    assert isinstance(linked_list.head_node, Node)
+    assert linked_list.head_node.get_value() == given_value
+    assert linked_list.head_node.get_next_node() is None
+
+
+# add_new_head()
+
+def test_add_new_head() -> None:
+    """add node from given value as new head node
+    """
+    value_1: int = 10
+    value_2: int = 20
+    linked_list: LinkedList = LinkedList(value_1)
+    linked_list.add_new_head(value_2)
+    assert isinstance(linked_list.head_node, Node)
+    assert linked_list.head_node.get_value() == value_2
+
+def test_link_old_head_to_new_head() -> None:
+    """set previous head node as next node of new head node if previous head node exists
+    """
+    value_1: int = 10
+    value_2: int = 20
+    linked_list: LinkedList = LinkedList(value_1)
+    linked_list.add_new_head(value_2)
+    head_node = linked_list.head_node
+    if head_node is None:
+        raise Exception('head_node is none')
+    next_node = head_node.next_node
+    if next_node is None:
+        raise Exception('next_node of head_node is none')
+    assert next_node.get_value() == value_1
+
+def test_link_none_to_new_head() -> None:
+    """set none as next node of new head node if does not exists
+    """
+    value_2: int = 20
+    linked_list: LinkedList = LinkedList()
+    linked_list.add_new_head(value_2)
+    head_node = linked_list.head_node
+    if head_node is None:
+        raise Exception('head_node is none')
+    assert head_node.get_next_node() is None
+
+def test_return_new_head_node() -> None:
+    """return newly added head node
+    """
+    value_1: int = 10
+    value_2: int = 20
+    linked_list: LinkedList = LinkedList(value_1)
+    result: Node = linked_list.add_new_head(value_2)
+    assert isinstance(result, Node)
+    assert result.get_value() == value_2
+    result_next_node = result.get_next_node()
+    if result_next_node is None:
+        raise Exception('next_node of head_node is none')
+    assert result_next_node.get_value() == value_1
+
+
+# append_node()
+
+def test_append_as_head_node() -> None:
+    """if list is empty, append node from given_value as head node
+    """
+    value_1: int = 10
+    linked_list: LinkedList = LinkedList()
+    linked_list.append_node(value_1)
+    head_node = linked_list.head_node
+    if head_node is None:
+        raise Exception('head_node is none')
+    assert head_node.get_value() == value_1
+
+def test_append_node() -> None:
+    """append node from given_value at the end of linked list
+    """
+    # arrange
+    value_1: int = 10
+    value_2: int = 20
+    value_3: int = 30
+    linked_list: LinkedList = LinkedList(value_1)
+    # act
+    linked_list.append_node(value_2)
+    linked_list.append_node(value_3)
+    # assert
+    previous_node = linked_list.get_head_node()
+    if previous_node is None:
+        raise Exception('head_node is None')
+    # head node
+    assert previous_node.get_value() == value_1
+    current_node = previous_node.get_next_node()
+    while current_node:
+        previous_node = current_node
+        current_node = current_node.get_next_node()
+    # tail node
+    assert previous_node.get_value() == value_3
+
+def test_return_appended_node() -> None:
+    """return appended node
+    """
+    # arrange
+    value_1: int = 10
+    value_2: int = 20
+    linked_list: LinkedList = LinkedList(value_1)
+    # act
+    result = linked_list.append_node(value_2)
+    assert isinstance(result, Node)
+    assert result.get_value() == value_2
+    assert result.get_next_node() is None
